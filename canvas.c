29ab9a07253e07737cea3d5a392adbd8da9b60c5
Elias <Elias.Haqiqat@protonmail.com>
2026-09-16 14:21:16 +0200

    Add constructor for pixel

--- canvas.c
+++ canvas.c
@@ -1,4 +1,5 @@
 #include "canvas.h"
+#include <stdint.h>
 #include <stdlib.h>
 
 canvas* canvas_create(int width, int height) {
@@ -9,6 +10,18 @@ canvas* canvas_create(int width, int height) {
     c->pixels = calloc(width * height, sizeof *c->pixels);
     if (!c->pixels) {free(c); return NULL;}
     return c;
+}
+
+pixel pixel_create(int red, int green, int blue) {
+    if (red < 0)        red = 0;
+    if (red > 255)      red = 255;
+    if (green < 0)      green = 0;
+    if (green > 255)    green = 255;
+    if (blue < 0)       blue = 0;
+    if (blue > 255)     blue = 255;
+
+    pixel p = {(uint8_t) red, (uint8_t) green, (uint8_t) blue};
+    return p;
 }
 
 void put_pixel(canvas *canvas, int x, int y, pixel colour) {
