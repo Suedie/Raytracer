f3e71eba6b1dbca09e20878c663e8eb28625b6b8
Elias <Elias.Haqiqat@protonmail.com>
2026-09-17 17:03:21 +0200

    Add header declarations for vector operations

--- vector.h
+++ vector.h
@@ -5,5 +5,9 @@ typedef struct {
     float x, y, z;
 } vector3;
 
+vector3 vector_add(vector3 a, vector3 b);
+vector3 vector_sub(vector3 a, vector3 b);
+vector3 vector_scale(float sclar, vector3 v);
+vector3 vector_dot_multiplication(vector3 a, vector3 b);
 
 #endif
