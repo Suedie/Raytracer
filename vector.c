49e64f4fba2e892c532f4c87821ead013c727ade
Elias <Elias.Haqiqat@protonmail.com>
2026-09-17 17:10:22 +0200

    Implement basic vector math

---
+++ vector.c
@@ -0,0 +1,35 @@
+#include "vector.h"
+#include "math.h"
+#include <math.h>
+
+vector3 vector_add(vector3 a, vector3 b) {
+    float x = a.x + b.x;
+    float y = a.y + b.y;
+    float z = a.z + b.z;
+
+    return (vector3) {x, y, z};
+}
+
+vector3 vector_sub(vector3 a, vector3 b) {
+    float x = a.x - b.x;
+    float y = a.y - b.y;
+    float z = a.z - b.z;
+
+    return (vector3) {x, y, z};
+}
+
+vector3 vector_scale(float scalar, vector3 v) {
+    float x = scalar * v.x;
+    float y = scalar * v.y;
+    float z = scalar * v.z;
+
+    return (vector3) {x, y, z};
+}
+
+float vector_dot(vector3 a, vector3 b) {
+    float x = a.x * b.x;
+    float y = a.y * b.y;
+    float z = a.z * b.z;
+
+    return (x + y + z);
+}
