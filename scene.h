8dbd8379163148e067610a92f88f808d381bdf97
Elias <Elias.Haqiqat@protonmail.com>
2026-09-23 23:06:42 +0200

    Add light variables to scene

--- scene.h
+++ scene.h
@@ -2,10 +2,17 @@
 #define SCENE_H
 
 #include "sphere.h"
+#include "light.h"
 
 typedef struct {
     sphere *spheres;
     int sphere_count;
+
+    point_light *points;
+    int point_light_count;
+    directional_light dir_light;
+    ambient_light ambient;
+
 } scene;
 
 scene *create_scene (void);
