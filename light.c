9a0e3233055f03cc92e002e8ec1e8cbaa6cfc3e7
Elias <Elias.Haqiqat@protonmail.com>
2026-09-24 02:09:36 +0200

    Define add point light

--- light.c
+++ light.c
@@ -10,4 +10,23 @@ lights create_lights(void) {
     ambient_light al;
 }
 
-void add_point_light(lights *l, point_light p);
+void add_point_light(lights *l, point_light p) {
+    if (l->point_light_count >= l->point_light_capacity) {
+        int new_cap;
+        if (l->point_light_capacity <= 0) {
+            new_cap = 4;
+        } else {
+            new_cap = l->point_light_capacity * 2;
+        }
+
+        point_light *new_point_lights = realloc(l->pl, new_cap * sizeof(*l->pl));
+        if (!new_point_lights) {
+            return;
+        }
+        l->pl = new_point_lights;
+        l->point_light_capacity = new_cap;
+    }
+
+    l->pl[l->point_light_count] = p;
+    l->point_light_count++;
+}
