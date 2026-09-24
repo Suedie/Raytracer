5f6d3f5e230b3b4878b5f4af294ab0cb06f58124
Elias <Elias.Haqiqat@protonmail.com>
2026-09-24 15:57:39 +0200

    Add yellow sphere

--- main.c
+++ main.c
@@ -18,14 +18,15 @@ int main () {
     sphere sphere_red       = (sphere) {(vector3) {0.0f, -1.0f, 3.0f}, 1.0f, pixel_create(255, 0, 0)};
     sphere sphere_blue      = (sphere) {(vector3) {2.0f, 0.0f, 4.0f}, 1.0f, pixel_create(0, 0, 255)};
     sphere sphere_green     = (sphere) {(vector3) {-2.0f, 0.0f, 4.0f}, 1.0f, pixel_create(0, 255, 0)};
+    sphere sphere_yellow    = (sphere) {(vector3) {0.0f, -5001.0f, 0.0f}, 5000.0f, pixel_create(255, 255, 0)};
 
     scene *scen = create_scene();
     if (!scen) return 1;
 
     add_sphere_to_scene(scen, sphere_red);
     add_sphere_to_scene(scen, sphere_blue);
-
     add_sphere_to_scene(scen, sphere_green);
+    add_sphere_to_scene(scen, sphere_yellow);
 
     point_light pl = {{2.0f, 1.0f, 0.0f}, 0.6f};
 
